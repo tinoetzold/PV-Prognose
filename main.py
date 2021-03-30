a1ef774925baff66bdc1e73f0f8eb6f9ee38fbd5
Tino Etzold <tino.etzold@kraftwerksschule.de>
2021-03-30 14:03:22 +0200

    added dhi and ghi calculation

--- main.py
+++ main.py
@@ -16,18 +16,44 @@ def main():
     mytimezone = config.get("SolarSystem", "MyTimezone", raw=True)
 
     # Solar parameter processing:
-    solar_proc = Solar_Processing(myaltitude, mylongitude, myaltitude, mytimezone)
+    solar_proc = Solar_Processing(mylatitude, mylongitude, myaltitude, mytimezone)
 
     
 
     # Now get the latest weather data:
     dwddata = dwd_fc.retrieve_data()
+    dwddata = dwddata.loc['2021-03-30 6:00':'2021-03-30 19:00']
 
     # Use the time range of the DWD Data as basis for further calculations
     time_range = dwddata.index
 
     # Now set up the weather data
     solar_proc.process_weather_data(time_range)
+
+    # Calc DNI using DISC model:
+    dni_disc = solar_proc.calc_dni_disc(time_range=time_range, ghi=dwddata.RAD_WH)
+
+    # Calc DNI using DIRINT model:
+    dni_dirint = solar_proc.calc_dni_dirindex(time_range=time_range, ghi=dwddata.RAD_WH, dew_point=dwddata.DEW_POINT_DEGC)
+    #print(dni_dirint)
+
+    # Calc DHI using the ERBS model
+    dhi_erbs = solar_proc.calc_dhi_erbs(ghi=dwddata.RAD_WH, time_range=time_range)
+    #print(dhi_erbs)
+
+    # Build up common dataframe:
+    whole_df = dwddata
+    whole_df.columns = whole_df.columns.add_categories(["DHI_ERBS", "DNI_DISC", "DNI_DIRINDEX"])
+    whole_df["DHI_ERBS"] = dhi_erbs["dhi"]
+    whole_df["DNI_DISC"] = dni_disc["dni"]
+    whole_df["DNI_DIRINDEX"] = dni_dirint.values
+
+    whole_df.columns = whole_df.columns.add_categories(["GHI_CLEARSKY", "DNI_CLEARSKY", "DHI_CLEARSKY"])
+    whole_df["GHI_CLEARSKY"] = solar_proc.clearsky.ghi
+    whole_df["DNI_CLEARSKY"] = solar_proc.clearsky.dni
+    whole_df["DHI_CLEARSKY"] = solar_proc.clearsky.dhi
+
+    whole_df.to_csv("new.csv")
 
 if __name__ == "__main__":
     main()
--- pv_forecast/solar_parameters.py
+++ pv_forecast/solar_parameters.py
@@ -1,5 +1,7 @@
+from datetime import time
 import pandas as pd
 import pvlib
+from pvlib.irradiance import erbs, disc, dirindex
 
 class Solar_Processing:
 
@@ -21,7 +23,7 @@ class Solar_Processing:
         
         self.clearsky = self.get_clearsky_weather(time_range)
         self.solpos = self.get_solar_postition(time_range)
-    
+   
     def get_clearsky_weather(self, time_range):
         """ Determine clearsky irradiation conditions. """
         return self.location.get_clearsky(time_range, model='ineichen')
@@ -33,7 +35,19 @@ class Solar_Processing:
                                                         longitude=self.mylongitude, 
                                                         altitude=self.myaltitude)
         return solpos
+    
+    def calc_dni_disc(self, time_range, ghi, mypressure=101325):
+        dni = disc(ghi=ghi, solar_zenith=self.solpos.zenith, datetime_or_doy=time_range)
+        return dni
 
+    def calc_dni_dirindex(self, time_range, ghi, mypressure=101325, dew_point=None):
+        dni = dirindex(dni_clearsky=self.clearsky.dni, ghi_clearsky=self.clearsky.ghi, ghi=ghi,
+                times=time_range, pressure=mypressure, zenith=self.solpos.zenith, temp_dew=dew_point)
+        return dni
+
+    def calc_dhi_erbs(self, time_range, ghi):
+        dhi = erbs(ghi=ghi, zenith=self.solpos.zenith, datetime_or_doy=time_range)
+        return dhi
 
 if __name__ ==  "__main__":
 
