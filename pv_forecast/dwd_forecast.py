48a4127b577d54952b8235779b26b2e95304de9e
Tino <tino.etzold@gmx.de>
2021-04-02 16:50:47 +0200

    changed data type of wind_speed

--- pv_forecast/dwd_forecast.py
+++ pv_forecast/dwd_forecast.py
@@ -10,8 +10,9 @@ https://www.dwd.de/DE/leistungen/met_verfahren_mosmix/met_verfahren_mosmix.html
 
 """
 import pandas as pd
-from wetterdienst.dwd.forecasts import DwdMosmixRequest, DwdMosmixType
-from wetterdienst.dwd.forecasts.metadata.dates import DwdForecastDate
+
+from wetterdienst.provider.dwd.forecast import DwdMosmixRequest, DwdMosmixType
+from wetterdienst.provider.dwd.forecast.metadata.dates import DwdForecastDate
 
 # Conversion factor: kJ/m^2 to Wh/m^2 (only valid for hourly time interval)
 J_to_KW = 0.277778
@@ -28,8 +29,8 @@ class DWD_Forecast:
         parameter=MOSMIX_ELEMENTS,
         start_issue=DwdForecastDate.LATEST,  # automatically set if left empty
         mosmix_type=DwdMosmixType.LARGE,     # SMALL (hourly) or LARGE (every 6 hours)
-        tidy_data=True,
-        humanize_parameters=True,
+        tidy=True,
+        humanize=True,
         )
         # Assign the station:
         self.stations = self.request.filter(station_id=station_id)
@@ -50,14 +51,15 @@ class DWD_Forecast:
         raw_data: pandas Dataframe having DWD Mosmix shape.
         """
         # First, create a table from plain DWD data using DATE as index
-        reshaped_data = pd.pivot_table(raw_data, values="VALUE", index=["DATE"], columns="PARAMETER", aggfunc='first')
+        reshaped_data = pd.pivot_table(raw_data, values="value", index=["date"], columns="parameter", aggfunc='first')
         # Add some columns in advance (otherwise will not work since columns are categorical)
-        reshaped_data.columns = reshaped_data.columns.add_categories(["TEMPERATURE_AIR_200DEGC", "DEW_POINT_DEGC", "RAD_WH"])
+        reshaped_data.columns = reshaped_data.columns.add_categories(["TEMPERATURE_AIR_200DEGC", "DEW_POINT_DEGC", "RAD_WH", "wind_speed"])
         # For pvlib, temperatures need to be available in degC:
         reshaped_data["TEMPERATURE_AIR_200DEGC"] = reshaped_data["TEMPERATURE_AIR_200"].apply(lambda x: x - 273.15)
         reshaped_data["DEW_POINT_DEGC"] = reshaped_data["TEMPERATURE_DEW_POINT_200"].apply(lambda x: x - 273.15)
         # Given global radiation is in kJ/m^2, tranform into Wh/m^2
         reshaped_data["RAD_WH"] = reshaped_data["RADIATION_GLOBAL"].apply(lambda x: x * J_to_KW)
+        reshaped_data['WIND_SPEED'] = reshaped_data['WIND_SPEED'].astype(float)
 
         return reshaped_data
 
@@ -65,4 +67,4 @@ class DWD_Forecast:
 if __name__ == "__main__":
     dwd_fc = DWD_Forecast("P0031")
     mydata = dwd_fc.retrieve_data()
-    print(mydata.columns)
+    print(mydata.WIND_SPEED)
